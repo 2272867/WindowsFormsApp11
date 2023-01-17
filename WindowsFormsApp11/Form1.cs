06e55d833a393db5d27b4bf4654d440654ecc2df
22728 <22728@DESKTOP-BBN3U2F>
2023-01-17 15:56:10 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp11/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        string photopath;
+        byte[] binaryphoto;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "jpg|*.jpg|Jpegs|*.Jpegs|png|*.png|GIf|*.Gif|Bitmaps|*.Bitmaps";
+            file.Title = "pls select an image";
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(file.OpenFile());
+                photopath = file.FileName;
+                FileStream fs = new FileStream(photopath, FileMode.Open, FileAccess.Read);
+                BinaryReader br = new BinaryReader(fs);
+                binaryphoto = br.ReadBytes((int)fs.Length);
+                fs.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
